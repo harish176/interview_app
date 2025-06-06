de484862e00fc1f706bbd8a9d0fcd9d7b30e6e11
harish <nharishreddyn6666gmail.com>
2025-06-06 15:52:52 +0530

    last commit

--- frontend/interview-prep-ai/src/utils/axiosInstance.js
+++ frontend/interview-prep-ai/src/utils/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {BASE_URL} from "./apiPaths.js"
+// import {BASE_URL} from "./apiPaths.js"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axionsInstance = axios.create({
     baseURL:BASE_URL,
@@ -10,7 +11,7 @@ const axionsInstance = axios.create({
     },
 });
 
-axionsInstance.interceptors.request.use(
+axiosInstance.interceptors.request.use(
     (config)=>{
         const accessToken = localStorage.getItem("token");
         if(accessToken){
